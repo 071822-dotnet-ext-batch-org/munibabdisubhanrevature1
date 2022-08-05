34eded4318f2526baec01d2b1ee24835007d4a91
Munib Abdisubhan <munib153@revature.net>
2022-08-05 13:08:28 -0500

    created SQL file

---
+++ codingChallenges/5_Operators/5_Operators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5_OperatorsChallenge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Increment(1));
+        }
+
+        /// <summary>
+        /// This method takes an int and returns the int incremented once
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int Increment(int num)
+        {
+            
+            return num++;
+            
+
+            //throw new NotImplementedException($"Increment() is not implemented yet.");
+        }
+
+       
+       
+       
+}
+}
+
